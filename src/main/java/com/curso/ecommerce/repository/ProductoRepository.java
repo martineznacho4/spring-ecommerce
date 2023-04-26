91aff86b1f5d7b2669bef7555b961eac4166391e
nacho <nachom@outlook.com.ar>
2023-04-26 19:13:32 -0300

    CRUD Producto

---
+++ src/main/java/com/curso/ecommerce/repository/ProductoRepository.java
@@ -0,0 +1,11 @@
+package com.curso.ecommerce.repository;
+
+import com.curso.ecommerce.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+
+
+}
