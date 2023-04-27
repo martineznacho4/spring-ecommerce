538f01ae746736c93f90752b731c0560825b8aca
nacho <nachom@outlook.com.ar>
2023-04-27 11:49:35 -0300

    vista de todos los productos

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -7,6 +7,7 @@ import com.curso.ecommerce.service.ProductoService;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,10 @@ public class ProductoController {
 
 
     @GetMapping("")
-    public String show(){
+    public String show(Model model){
+
+        model.addAttribute("productos",productoService.findAll());
+
         return "productos/show";
     }
 
