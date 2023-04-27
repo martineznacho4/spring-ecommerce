8016293d8739f81cad42b0b06cf5194d06d3e981
nacho <nachom@outlook.com.ar>
2023-04-27 12:58:23 -0300

    agregadas las funcinalidades de delete y update

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -9,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/productos")
@@ -45,4 +48,34 @@ public class ProductoController {
         productoService.save(producto);
         return "redirect:/productos";
     }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model){
+
+        Producto producto = new Producto();
+        Optional<Producto> optionalProducto = productoService.get(id);
+        producto = optionalProducto.get();
+
+        LOGGER.info("Producto buscado {}", producto);
+        model.addAttribute("producto", producto);
+
+        return "productos/edit";
+    };
+
+    @PostMapping("/update")
+    public String update(Producto producto){
+
+        productoService.update(producto);
+
+        return"redirect:/productos";
+    };
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id){
+
+        productoService.delete(id);
+        return "redirect:/productos";
+    };
+
+
 }
