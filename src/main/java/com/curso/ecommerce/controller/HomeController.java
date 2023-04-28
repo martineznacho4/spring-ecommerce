5939765e4adde6d85d0f987959c4c00479a64c2e
nacho <nachom@outlook.com.ar>
2023-04-28 10:24:50 -0300

    vista del producto al usuario

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +21,11 @@ public class HomeController {
 
         model.addAttribute("productos",productoService.findAll() );
         return "usuario/home";
+    };
+
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id){
+
+        return "usuario/productohome";
     };
 }
